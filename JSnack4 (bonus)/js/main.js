639938ae466b2d3ecc8d89e34bfab576f744fa74
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-27 19:18:12 +0100

    fixes es.4

--- JSnack4 (bonus)/js/main.js
+++ JSnack4 (bonus)/js/main.js
@@ -4,9 +4,12 @@
 // SVOLGIMENTO:
 // Crea array 1
 const myArray1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+let outputMessage = document.getElementById("output_message");
+let initMessage = document.getElementById("init_message");
 
 // Crea array 2 con meno elementi dell'array 1
 let myArray2 = ["a", "b", "c"];
+initMessage.innerHTML = "L'array iniziale è: " + myArray2;
 
 // SE array 2 ha meno elementi dell array 1 aggiungi elementi a array 2
 for (let i = 0; i < myArray1.length && myArray1.length != myArray2.length; i++) {
@@ -16,3 +19,5 @@ for (let i = 0; i < myArray1.length && myArray1.length != myArray2.length; i++) 
     }
     console.log(myArray2);
 }
+
+outputMessage.innerHTML = "L'array aggiornato sara cosi: composto: " + myArray2
