d61ebabc397b5592c3a7a079d7b728f1d2c4fe75
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-27 18:57:59 +0100

    fixes es.1

--- JSnack1/js/main.js
+++ JSnack1/js/main.js
@@ -2,17 +2,24 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 // SVOLGIMENTO:
-// Inserisci numero
-let myNumber = parseInt(prompt("Scegli un numero?"));
-// SE numero pari
-if (myNumber % 2 == 0) {
-    // stampi numero
-    myOutputNumber = myNumber;
-    console.log(myOutputNumber);
-    // ALTRIMENTI stampi numero successivo
-} else {
-    myOutputNumber = myNumber + 1;
-    console.log(myOutputNumber);
-}
+const generateButton = document.getElementById("generate_button");
 
-document.getElementById("output_number").innerHTML = myOutputNumber;
+generateButton.addEventListener(
+    "click",
+    function () {
+        // Inserisci numero
+        const inputNumber = document.getElementById("input_number");
+        let myNumber = parseInt(inputNumber.value);
+        let myOutputNumber;
+        // SE numero pari
+        if (myNumber % 2 == 0) {
+            // stampi numero
+            myOutputNumber = myNumber;
+            // ALTRIMENTI stampi numero successivo
+        } else {
+            myOutputNumber = myNumber + 1;
+        }
+
+        document.getElementById("output_number").innerHTML = myOutputNumber;
+    }
+)
